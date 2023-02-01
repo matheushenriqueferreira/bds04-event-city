e5510e2e55e422234cea44ae651cd44170172b41
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-02-01 15:08:16 -0300

    Data validation, Insert new event, EventControllerIT tests passed

--- src/main/java/com/devsuperior/bds04/controllers/EventController.java
+++ src/main/java/com/devsuperior/bds04/controllers/EventController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/events")
@@ -21,5 +23,12 @@ public class EventController {
     public ResponseEntity<Page<EventDTO>> findAll(Pageable pageable) {
         Page<EventDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventDTO> insert(@RequestBody @Valid EventDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
--- src/main/java/com/devsuperior/bds04/services/EventService.java
+++ src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.bds04.services;
 
 import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.City;
 import com.devsuperior.bds04.entities.Event;
 import com.devsuperior.bds04.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,16 @@ public class EventService {
     public Page<EventDTO> findAll(Pageable pageable) {
         Page<Event> result = repository.findAll(pageable);
         return result.map(event -> new EventDTO(event.getId(), event.getName(), event.getDate(), event.getUrl(), event.getCity().getId()));
+    }
+
+    @Transactional
+    public EventDTO insert(EventDTO dto) {
+        Event entity = new Event();
+        entity.setName(dto.getName());
+        entity.setDate(dto.getDate());
+        entity.setUrl(dto.getUrl());
+        entity.setCity(new City(dto.getCityId(), null));
+        entity = repository.save(entity);
+        return new EventDTO(entity);
     }
 }
